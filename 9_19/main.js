043bddc989ad729ac4a8655922ada243332b5240
takumi80 <mrkmtkt0927@gmail.com>
2024-09-20 04:50:22 +0000

    9月19日分を別ファイルに移動

---
+++ 9_19/main.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").textContent="村上"
+document.querySelector("h2").textContent="村上"
+
+//documentとは！！
+/* htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM(ドム)
+/* ドキュメントオブジェクトモデル(Document Object Model) の略でWebページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+
+//上記コードの意味
+//document.querySelrcutor("h1").textContent = "自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//document.querySelrcutor("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent = "自分の名前"
