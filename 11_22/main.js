126c7ca5444c3a805657419245bd44ef107f31c4
takumi80 <mrkmtkt0927@gmail.com>
2024-11-22 06:17:59 +0000

    11_22

---
+++ 11_22/main.js
@@ -0,0 +1,27 @@
+const gameSelector = document.getElementById('game-selector')
+const gameContainer = document.getElementById('game-container')
+
+ gameContainer.textContent="ゲームを選ぶとここに表示されます"
+
+gameSelector.addEventListener("change",function(){
+   gameContainer.innerHTML = "";
+   switch(gameSelector.value){
+      case "none":
+         gameContainer.textContent="ゲームを選ぶとここに表示されます"
+         break;
+      case "click-counter":
+         ClickCounterGame();
+         break;
+      case "number-guess":
+         NumberGuessGame();
+         break;
+       case "RPS":
+         RPS();
+         break;
+   }
+})
+
+
+
+
+
