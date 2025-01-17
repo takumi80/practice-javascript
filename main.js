6a893b100b1c81d25c18690f290ce699af6d0650
takumi80 <mrkmtkt0927@gmail.com>
2025-01-17 06:17:54 +0000

    数当てゲームの追加

--- main.js
+++ main.js
@@ -15,12 +15,22 @@ function ClickCounterGame(){
     counter.textContent = count;
 
     button1.addEventListener("click",function(){
+        if(count < 100){
         count++;
+        }
         counter.textContent = count;
     })
 
     button2.addEventListener("click",function(){
-        count = count + 10;
+        if(count <= 90){
+            count = count + 10; 
+        }
+        counter.textContent = count;
+
+    })
+
+    button3.addEventListener("click",function(){
+        count = 0;
         counter.textContent = count;
     })
 
@@ -35,3 +45,17 @@ function ClickCounterGame(){
     gameContainer.appendChild(counter);
 }
 ClickCounterGame();
+
+
+function stertNumberGuessGame(){
+    const gameContainer = document.getElementById("game-container");
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    let message = document.createElement('p');
+    message.textContent = randomNumber
+    let input = document.createElement('input');
+    input.type = 'number';
+    input.placeholder = "好きな数字を入力してください(1~100)"
+    gameContainer.appendChild(message);
+    gameContainer.appendChild(input);
+}
+stertNumberGuessGame();
